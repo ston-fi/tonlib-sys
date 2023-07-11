f065fb25e576ecd79a992372187e8b438123abe3
dbaranov <baranov34@gmail.com>
2023-07-11 15:19:05 +0400

    wip

--- build.rs
+++ build.rs
@@ -181,9 +181,6 @@ fn build() {
         println!("cargo:rustc-link-lib=dylib=stdc++");
     }
 
-    //    println!("cargo:rustc-link-lib=static=tonlibjson");
-    println!("cargo:rustc-link-lib=static=tonlib");
-    println!("cargo:rustc-link-lib=static=emulator_static");
 }
 
 #[cfg(feature = "shared-tonlib")]
