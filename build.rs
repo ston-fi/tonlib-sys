9f39ece07758c8bfb6e9296a0b7a2be5c302cdeb
dbaranov <baranov34@gmail.com>
2023-07-12 20:21:48 +0400

    wip

--- build.rs
+++ build.rs
@@ -85,6 +85,7 @@ fn build() {
     }
     let dst = cmake::Config::new("ton")
         .define("TON_ONLY_TONLIB", "ON")
+        .define("USE_EMSCRIPTEN","111")
         .define("BUILD_SHARED_LIBS", "OFF")
         .configure_arg("-Wno-dev")
         .build_target("tonlibjson")
