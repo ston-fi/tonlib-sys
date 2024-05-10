be698c3c1a2676e9467e5e2cf43266f1c22adf8e
dbaranovstonfi <136370214+dbaranovstonfi@users.noreply.github.com>
2024-05-10 13:48:27 +0400

    Fix clang build on mac (#20)
    make no_avx works with gcc
    
    ---------
    Authored-by: Dmitrii Korchagin <d.korchagin@ston.fi>

--- build.rs
+++ build.rs
@@ -152,7 +152,7 @@ fn build_tonlibjson() {
         .very_verbose(false);
 
     #[cfg(feature = "no_avx512")]
-    disable_avx512_for_clang(dst);
+    disable_avx512_for_gcc(dst);
 
     if cfg!(target_os = "macos") {
         let brew_prefix_output = Command::new("brew").arg("--prefix").output().unwrap();
@@ -288,7 +288,7 @@ fn build_emulator() {
         .very_verbose(false);
 
     #[cfg(feature = "no_avx512")]
-    disable_avx512_for_clang(dst);
+    disable_avx512_for_gcc(dst);
 
     let dst = dst.build();
 
@@ -306,14 +306,20 @@ fn build() {
     println!("cargo:rustc-link-lib=tonlibjson.0.5");
 }
 
+// for clang we just ignore unknown instructions
 #[cfg(feature = "no_avx512")]
-fn disable_avx512_for_clang(dst: &mut Config) -> &mut Config {
-    dst
-    .define("CMAKE_C_FLAGS", "-mno-avx512f -mno-avx512dq -mno-avx512cd -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-vpclmulqdq")
-    .define("CMAKE_CXX_FLAGS", "-mno-avx512f -mno-avx512dq -mno-avx512cd -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-vpclmulqdq")
-    .define("CMAKE_C_FLAGS_RELEASE", "-mno-avx512f -mno-avx512dq -mno-avx512cd -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-vpclmulqdq")
-    .define("CMAKE_CXX_FLAGS_RELEASE", "-mno-avx512f -mno-avx512dq -mno-avx512cd -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-vpclmulqdq")
-    .asmflag("-mno-avx512f -mno-avx512dq -mno-avx512cd -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-vpclmulqdq")
+fn disable_avx512_for_gcc(dst: &mut Config) -> &mut Config {
+    let disable_avx512 = "-mno-avx512f -mno-avx512dq -mno-avx512cd -mno-avx512bw -mno-avx512vl -mno-avx512ifma -mno-avx512vbmi -mno-vpclmulqdq";
+    let compiler_flags = format!("-Wno-unused-command-line-argument {}", disable_avx512);
+    for flag in &[
+        "CMAKE_C_FLAGS",
+        "CMAKE_CXX_FLAGS",
+        "CMAKE_C_FLAGS_RELEASE",
+        "CMAKE_CXX_FLAGS_RELEASE",
+    ] {
+        dst.define(flag, compiler_flags.as_str());
+    }
+    dst.asmflag(disable_avx512)
 }
 
 #[cfg(feature = "no_avx512")]
