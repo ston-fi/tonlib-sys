d88f8098167f8c499d9dcf2b8800763374eb6096
dbaranov34 <baranov34@gmail.com>
2025-02-12 21:44:24 +0400

    wip

--- build.rs
+++ build.rs
@@ -22,151 +22,19 @@ fn build() {
 
     #[cfg(feature = "no_avx512")]
     disable_avx512_for_rustc();
-    env::set_var("TON_MONOREPO_REVISION", TON_MONOREPO_REVISION);
-    println!("cargo:rerun-if-env-changed=TON_MONOREPO_REVISION");
-    println!("cargo:rerun-if-changed=build.rs");
+    checkout();
 
-    // cleanup tonlib after previous build
-    if Path::new(TON_MONOREPO_DIR).exists() {
-        let _ = fs::remove_dir_all(TON_MONOREPO_DIR);
-    }
-
-    let clone_status = Command::new("git")
-        .args([
-            "clone",
-            "--branch",
-            TON_MONOREPO_REVISION,
-            "--depth",
-            "1",                    // get only the latest commit
-            "--recurse-submodules", // clone submodules as well
-            "--shallow-submodules", // get only the latest commit of submodules
-            "https://github.com/ton-blockchain/ton",
-            TON_MONOREPO_DIR,
-        ])
-        .status()
-        .unwrap();
-
-    if !clone_status.success() {
-        // fallback to clone entire repo and then checkout desired commit
-        let full_clone_status = Command::new("git")
-            .args([
-                "clone",
-                "--recurse-submodules", // clone submodules as well
-                "--shallow-submodules", // get only the latest commit of submodules
-                "https://github.com/ton-blockchain/ton",
-                TON_MONOREPO_DIR,
-            ])
-            .status()
-            .unwrap();
-
-        if full_clone_status.success() {
-            println!("Cloned repository successfully!");
-        } else {
-            panic!("Failed to clone repository!");
-        }
-    };
-
-    let checkout_status = Command::new("git")
-        .current_dir(TON_MONOREPO_DIR)
-        .args(["checkout", TON_MONOREPO_REVISION])
-        .status()
-        .unwrap();
-
-    if checkout_status.success() {
-        println!("Cloned and checked out specific commit successfully!");
-    } else {
-        panic!("Failed to checkout specific commit!");
-    }
-
-    let update_submodules_status = Command::new("git")
-        .current_dir(TON_MONOREPO_DIR)
-        .args(["submodule", "update", "--init", "--recursive"])
-        .status()
-        .unwrap();
-    if !update_submodules_status.success() {
-        panic!("Git update submodules for TON repo fail");
-    }
+    let mut dep_paths = HashMap::new();
 
     if cfg!(target_os = "macos") {
-        if Command::new("brew").args(["-h"]).output().is_err() {
-            panic!("brew is not available. Please install it to proceed");
-        }
-        let mut dep_paths = HashMap::new();
-        for dep in &["openssl@3", "lz4", "pkgconfig", "libsodium", "secp256k1"] {
-            let dep_installed = Command::new("brew")
-                .args(["--prefix", dep])
-                .output()
-                .unwrap();
-            if !dep_installed.status.success() {
-                panic!(
-                    "{} is not installed. To install: `brew install {}`",
-                    dep, dep
-                );
-            } else {
-                dep_paths.insert(
-                    dep.to_string(),
-                    std::str::from_utf8(dep_installed.stdout.as_slice())
-                        .unwrap()
-                        .trim()
-                        .to_string(),
-                );
-            }
-        }
-
-        let openssl = &dep_paths["openssl@3"];
-        let libsodium = &dep_paths["libsodium"];
-        let secp256k1 = &dep_paths["secp256k1"];
-
-        env::set_var("OPENSSL_ROOT_DIR", openssl);
-        env::set_var("OPENSSL_INCLUDE_DIR", format!("{openssl}/include"));
-        env::set_var("OPENSSL_CRYPTO_LIBRARY", format!("{openssl}/lib"));
-        env::set_var("CXX", "clang++");
-
-        println!("cargo:rustc-link-search=native={openssl}/lib");
-        println!("cargo:rustc-link-search=native={libsodium}/lib");
-        println!("cargo:rustc-link-search=native={secp256k1}/lib");
+        check_brew();
+        dep_paths = get_brew_paths(&["openssl@3", "lz4", "pkgconfig", "libsodium", "secp256k1"]);
+    } else if cfg!(target_os = "linux") {
+        check_pkg_config();
+        dep_paths = get_pkg_config_paths(&["openssl", "liblz4", "libsodium", "libsecp256k1"]);
     }
-    else{
-         // Check if `pkg-config` is installed
-         if Command::new("pkg-config").args(["--version"]).output().is_err() {
-            panic!("pkg-config is not installed. Please install it using your package manager.");
-        }
 
-        let mut dep_paths = HashMap::new();
-        for dep in &["openssl", "liblz4", "libsodium", "libsecp256k1"] {
-                 let dep_installed = Command::new("pkg-config")
-                .args(["--modversion", dep])
-                .output()
-                .expect("Failed to run pkg-config");
-            if !dep_installed.status.success() {
-                panic!(
-                    "{} is not installed. To install: `sudo apt install {}`",
-                    dep, dep
-                );
-            } else {
-                dep_paths.insert(
-                    dep.to_string(),
-                    std::str::from_utf8(dep_installed.stdout.as_slice())
-                        .unwrap()
-                        .trim()
-                        .to_string(),
-                );
-            }
-        }
-
-        let openssl = &dep_paths["openssl"];
-        let libsodium = &dep_paths["libsodium"];
-        let secp256k1 = &dep_paths["libsecp256k1"];
-
-        env::set_var("OPENSSL_ROOT_DIR", openssl);
-        env::set_var("OPENSSL_INCLUDE_DIR", format!("{openssl}/include"));
-        env::set_var("OPENSSL_CRYPTO_LIBRARY", format!("{openssl}/lib"));
-        env::set_var("CXX", "clang++");
-
-        println!("cargo:rustc-link-search=native={openssl}/lib");
-        println!("cargo:rustc-link-search=native={libsodium}/lib");
-        println!("cargo:rustc-link-search=native={secp256k1}/lib");      
-    }
+    set_env_vars(&dep_paths);
 
     env::set_var("LD_LIBRARY_PATH", "lib/x86_64-linux-gnu");
     build_tonlibjson(cmake_build_type);
@@ -307,23 +175,23 @@ fn build_tonlibjson(cmake_build_type: &str) {
     // println!("cargo:rustc-link-lib=sodium");
     // println!("cargo:rustc-link-lib=crypto");
 
-    // println!(
-    //     "cargo:rustc-link-search=native={}/build/tonlib",
-    //     dst.display()
-    // );
-    // println!("cargo:rustc-link-lib=static=tonlibjson");
-    // println!("cargo:rustc-link-lib=static=tonlib");
-    // println!("cargo:rustc-link-lib=static=tonlibjson_private");
+    println!(
+        "cargo:rustc-link-search=native={}/build/tonlib",
+        dst.display()
+    );
+    println!("cargo:rustc-link-lib=static=tonlibjson");
+    println!("cargo:rustc-link-lib=static=tonlib");
+    println!("cargo:rustc-link-lib=static=tonlibjson_private");
 }
 
 fn build_emulator(cmake_build_type: &str) {
     let mut cfg = Config::new(TON_MONOREPO_DIR);
     let mut dst = cfg
         .configure_arg("-DTON_ONLY_TONLIB=true")
-        .configure_arg("-DBUILD_SHARED_LIBS=false")
+        .configure_arg("-DBUILD_SHARED_LIBS=true")
        //.define("OPENSSL_USE_STATIC_LIBS", "1")
         .define("TON_ONLY_TONLIB", "ON")
-        .define("BUILD_SHARED_LIBS", "OFF")
+        .define("BUILD_SHARED_LIBS", "ON")
         .define("PORTABLE", "1")
         .define("CMAKE_BUILD_TYPE", cmake_build_type)
         .define("CMAKE_C_FLAGS", "-w")
@@ -510,4 +378,171 @@ fn disable_avx512_for_gcc(dst: &mut Config) -> &mut Config {
 #[cfg(feature = "no_avx512")]
 fn disable_avx512_for_rustc() {
     println!("cargo:rustc-env=RUSTFLAGS=-C target-feature=-avx512f,-avx512dq,-avx512cd,-avx512bw,-avx512vl,-avx512ifma,-avx512vbmi,-vpclmulqdq");
+}
+
+/// Check if Homebrew is installed (macOS)
+fn check_brew() {
+    if Command::new("brew").args(["-h"]).output().is_err() {
+        panic!("brew is not available. Please install it to proceed.");
+    }
+}
+
+/// Get dependency paths from Homebrew
+fn get_brew_paths(deps: &[&str]) -> HashMap<String, String> {
+    let mut paths = HashMap::new();
+
+    for dep in deps {
+        let dep_installed = Command::new("brew")
+            .args(["--prefix", dep])
+            .output()
+            .expect("Failed to run brew command");
+
+        if !dep_installed.status.success() {
+            panic!(
+                "{} is not installed. To install: `brew install {}`",
+                dep, dep
+            );
+        } else {
+            paths.insert(
+                dep.to_string(),
+                String::from_utf8(dep_installed.stdout)
+                    .unwrap()
+                    .trim()
+                    .to_string(),
+            );
+        }
+    }
+
+    paths
+}
+
+/// Check if `pkg-config` is installed (Linux)
+fn check_pkg_config() {
+    if Command::new("pkg-config")
+        .args(["--version"])
+        .output()
+        .is_err()
+    {
+        panic!("pkg-config is not installed. Please install it using your package manager.");
+    }
+}
+
+/// Get dependency paths using `pkg-config`
+fn get_pkg_config_paths(deps: &[&str]) -> HashMap<String, String> {
+    let mut paths = HashMap::new();
+
+    for dep in deps {
+        let dep_installed = Command::new("pkg-config")
+            .args(["--modversion", dep])
+            .output()
+            .expect("Failed to run pkg-config");
+
+        if !dep_installed.status.success() {
+            panic!(
+                "{} is not installed. Install it with: `sudo apt install {}-dev`",
+                dep, dep
+            );
+        } else {
+            paths.insert(
+                dep.to_string(),
+                String::from_utf8(dep_installed.stdout)
+                    .unwrap()
+                    .trim()
+                    .to_string(),
+            );
+        }
+    }
+
+    paths
+}
+
+/// Set environment variables and linker flags
+fn 
+set_env_vars(dep_paths: &HashMap<String, String>) {
+    let openssl = dep_paths
+        .get("openssl")
+        .or(dep_paths.get("openssl@3"))
+        .expect("OpenSSL not found");
+    let libsodium = dep_paths.get("libsodium").expect("libsodium not found");
+    let secp256k1 = dep_paths
+        .get("libsecp256k1")
+        .or(dep_paths.get("secp256k1"))
+        .expect("secp256k1 not found");
+
+    env::set_var("OPENSSL_ROOT_DIR", openssl);
+    env::set_var("OPENSSL_INCLUDE_DIR", format!("{}/include", openssl));
+    env::set_var("OPENSSL_CRYPTO_LIBRARY", format!("{}/lib", openssl));
+    env::set_var("CXX", "clang++");
+
+    println!("cargo:rustc-link-search=native={}/lib", openssl);
+    println!("cargo:rustc-link-search=native={}/lib", libsodium);
+    println!("cargo:rustc-link-search=native={}/lib", secp256k1);
+}
+
+
+fn checkout(){
+    env::set_var("TON_MONOREPO_REVISION", TON_MONOREPO_REVISION);
+    println!("cargo:rerun-if-env-changed=TON_MONOREPO_REVISION");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // cleanup tonlib after previous build
+    if Path::new(TON_MONOREPO_DIR).exists() {
+        let _ = fs::remove_dir_all(TON_MONOREPO_DIR);
+    }
+
+    let clone_status = Command::new("git")
+        .args([
+            "clone",
+            "--branch",
+            TON_MONOREPO_REVISION,
+            "--depth",
+            "1",                    // get only the latest commit
+            "--recurse-submodules", // clone submodules as well
+            "--shallow-submodules", // get only the latest commit of submodules
+            "https://github.com/ton-blockchain/ton",
+            TON_MONOREPO_DIR,
+        ])
+        .status()
+        .unwrap();
+
+    if !clone_status.success() {
+        // fallback to clone entire repo and then checkout desired commit
+        let full_clone_status = Command::new("git")
+            .args([
+                "clone",
+                "--recurse-submodules", // clone submodules as well
+                "--shallow-submodules", // get only the latest commit of submodules
+                "https://github.com/ton-blockchain/ton",
+                TON_MONOREPO_DIR,
+            ])
+            .status()
+            .unwrap();
+
+        if full_clone_status.success() {
+            println!("Cloned repository successfully!");
+        } else {
+            panic!("Failed to clone repository!");
+        }
+    };
+
+    let checkout_status = Command::new("git")
+        .current_dir(TON_MONOREPO_DIR)
+        .args(["checkout", TON_MONOREPO_REVISION])
+        .status()
+        .unwrap();
+
+    if checkout_status.success() {
+        println!("Cloned and checked out specific commit successfully!");
+    } else {
+        panic!("Failed to checkout specific commit!");
+    }
+
+    let update_submodules_status = Command::new("git")
+        .current_dir(TON_MONOREPO_DIR)
+        .args(["submodule", "update", "--init", "--recursive"])
+        .status()
+        .unwrap();
+    if !update_submodules_status.success() {
+        panic!("Git update submodules for TON repo fail");
+    }
 }
