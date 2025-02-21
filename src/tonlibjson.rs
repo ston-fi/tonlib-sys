68775a78bc78b7a082a69821a115e1240ed716ff
Dmitrii Korchagin <d.korchagin@ston.fi>
2025-02-21 09:17:52 +0000

    Impl #BE-2091: use ton v2025.02

--- src/tonlibjson.rs
+++ src/tonlibjson.rs
@@ -1,22 +1,22 @@
 extern "C" {
-    pub fn tonlib_client_json_create() -> *mut ::std::os::raw::c_void;
+    pub fn tonlib_client_json_create() -> *mut std::os::raw::c_void;
 
     pub fn tonlib_client_json_send(
-        client: *mut ::std::os::raw::c_void,
-        request: *const ::std::os::raw::c_char,
+        client: *mut std::os::raw::c_void,
+        request: *const std::os::raw::c_char,
     );
 
     pub fn tonlib_client_json_receive(
-        client: *mut ::std::os::raw::c_void,
+        client: *mut std::os::raw::c_void,
         timeout: f64,
-    ) -> *const ::std::os::raw::c_char;
+    ) -> *const std::os::raw::c_char;
 
     pub fn tonlib_client_json_execute(
-        client: *mut ::std::os::raw::c_void,
-        request: *const ::std::os::raw::c_char,
-    ) -> *const ::std::os::raw::c_char;
+        client: *mut std::os::raw::c_void,
+        request: *const std::os::raw::c_char,
+    ) -> *const std::os::raw::c_char;
 
-    pub fn tonlib_client_json_destroy(client: *mut ::std::os::raw::c_void);
+    pub fn tonlib_client_json_destroy(client: *mut std::os::raw::c_void);
 
     pub fn tonlib_client_set_verbosity_level(verbosity_level: u32);
 }
